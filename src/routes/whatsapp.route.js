e19af267df76f25a65de5a47c8c2faa25164893e
KaykyOne <kaykyzioti@gmail.com>
2026-01-29 09:01:24 -0300

    minor fix router

--- src/routes/whatsapp.route.js
+++ src/routes/whatsapp.route.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { sendMessage, clearMessages, startWhatsappBot, deleteScheduledMessagesForPhone } from "../controller/whatsapp.controller.js";
+import { sendMessage, clearMessages, startWhatsappBot, deleteScheduledMessages } from "../controller/whatsapp.controller.js";
 
 const router = Router();
 
 router.post("/", sendMessage);
 router.get("/start", startWhatsappBot);
 router.delete("/clearAll", clearMessages);
-router.delete("/clearScheduled/:phone", deleteScheduledMessagesForPhone);
+router.delete("/clearScheduled/:phone", deleteScheduledMessages);
 
 export default router;
